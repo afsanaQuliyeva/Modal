dfdd322472b37156187ed94c40825e2e892f197f
efsaneGuliyeva <efsanequliyeva@outlook.com>
2021-05-31 21:23:37 +0400

    Final Version

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const openModalBtn = document.querySelector('[data-modal-target]');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.modal-close-btn');
 
 openModalBtn.addEventListener('click', ()=>{
     const modal = document.querySelector(openModalBtn.dataset.modalTarget);
@@ -7,6 +9,25 @@ openModalBtn.addEventListener('click', ()=>{
     console.log(openModalBtn.dataset.modalTarget)
 })
 
+closeModalBtn.addEventListener('click', () => {
+    const modal = closeModalBtn.closest('.modal');
+    closeModal(modal);
+})
+
+
+overlay.addEventListener('click', () => {
+    const modal = document.querySelector('.modal.active');
+    closeModal(modal);
+})
+
+/*Functions*/
+
 function openModal(modal) {
     modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    modal.classList.remove('active')
+    overlay.classList.remove('active');
 }
